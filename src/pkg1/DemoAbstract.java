177dd14a1f681eda7059f03644366dce80782090
Hitesh <hkshitesh@gmail.com>
2022-02-26 10:30:03 +0530

    Second Commit

---
+++ src/pkg1/DemoAbstract.java
@@ -0,0 +1,19 @@
+package pkg1;
+ abstract class C1
+{
+	public void f1()
+	{
+		
+	}
+	abstract public void f2();
+}
+
+public class DemoAbstract {
+
+	public static void main(String[] args) 
+	{
+		C1 ob = new C1();	
+
+	}
+
+}
